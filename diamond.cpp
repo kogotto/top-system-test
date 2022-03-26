e8e4aa8e7e1ab1dc66dbd3156642bcb0a6ada6cc
Yakov Didenko <kogotto@gmail.com>
2022-03-26 20:50:29 +0300

    draw actual diamond

--- diamond.cpp
+++ diamond.cpp
@@ -1,6 +1,52 @@
 #include "diamond.h"
 
 #include <iostream>
+#include <iomanip>
+
+namespace {
+
+constexpr char straight = '/';
+constexpr char reverse = '\\';
+
+void topLine(int size, int row) {
+    const int len = size * 2;
+    const int between = row * 2;
+    const int before = (len - between - 2) / 2;
+
+    std::cout
+        << std::setw(before) << ""
+        << straight
+        << std::setw(between) << ""
+        << reverse
+        << '\n';
+}
+
+void drawTop(int size) {
+    for (int row = 0; row < size; ++row) {
+        topLine(size, row);
+    }
+}
+
+void bottomLine(int size, int row) {
+    const int len = size * 2;
+    const int before = row;
+    const int between = len - 2 * before - 2;
+
+    std::cout
+        << std::setw(before) << ""
+        << reverse
+        << std::setw(between) << ""
+        << straight
+        << '\n';
+}
+
+void drawBottom(int size) {
+    for (int row = 0; row < size; ++row) {
+        bottomLine(size, row);
+    }
+}
+
+}
 
 Diamond::Diamond(int size)
     : size(size) {}
@@ -10,5 +56,6 @@ void Diamond::info() const {
 }
 
 void Diamond::draw() const {
-    info();
+    drawTop(size);
+    drawBottom(size);
 }
