56a2df52ec70ed0362825524444f79e15d88bcb8
Yakov Didenko <kogotto@gmail.com>
2022-03-26 16:28:24 +0300

    draw actual square

--- square.cpp
+++ square.cpp
@@ -2,6 +2,44 @@
 
 #include <iostream>
 
+namespace {
+
+constexpr char corner = '+';
+constexpr char horizontal = '-';
+constexpr char vertical = '|';
+constexpr char inner = ' ';
+
+void top(int size) {
+    std::cout << corner;
+    for (int i = 2; i < size; ++i) {
+        std::cout << horizontal;
+    }
+    std::cout << corner << '\n';
+}
+
+inline void bottom(int size) {
+    top(size);
+}
+
+void middle(int size) {
+    std::cout << vertical;
+    for (int i = 2; i < size; ++i) {
+        std::cout << inner;
+    }
+    std::cout << vertical << '\n';
+}
+
+void drawSquare(int size) {
+    top(size);
+    for (int i = 2; i < size; ++i) {
+        middle(size);
+    }
+    bottom(size);
+    std::cout << '\n';
+}
+
+}
+
 Square::Square(int size)
     : size(size) {}
 
@@ -10,5 +48,5 @@ void Square::info() const {
 }
 
 void Square::draw() const {
-    info();
+    drawSquare(size);
 }
